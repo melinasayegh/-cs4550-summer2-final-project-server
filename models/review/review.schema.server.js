057137a46a2fa84808d2801021369b2e20173575
Melina <sayeghmelina@gmail.com>
2018-08-11 16:54:11 -0400

    review schema

---
+++ models/review/review.schema.server.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = mongoose.Schema({
+    text: String,
+    createdAt: Date,
+    updatedAt: Date,
+    user: {
+        type: mongoose.Schema,
+        ref: 'UserModel'
+    },
+    recipe: {
+        type: mongoose.Schema,
+        ref: 'RecipeModel'
+    }
+
+}, {collection: 'review'});
+
+module.exports = reviewSchema;
