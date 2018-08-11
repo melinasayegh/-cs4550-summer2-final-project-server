d7831c1bd939072f8264a13a4c7c97fbedfcd5f8
Melina <sayeghmelina@gmail.com>
2018-08-11 16:51:40 -0400

    recipe schema

---
+++ models/recipe/recipe.schema.server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    creator: User,
+    createdAt: Date,
+    updatedAt: Date,
+    ingredients: String,
+    directions: String,
+    prepTime: Number,
+    cookTime: Number,
+    numServings: Number,
+    tags: [{
+        type: String,
+        enum: ['VEGAN', 'VEGAN', 'DIABETIC', 'GLUTEN-FREE', 'LOW-CALORIE', 'GUILTY-PLEASURE'],
+        default: 'VEGETARIAN'
+    }],
+    reviews: [{
+        type: mongoose.Schema,
+        ref: 'ReviewModel'
+    }]
+
+}, {collection: 'recipe'});
+
+module.exports = recipeSchema;
