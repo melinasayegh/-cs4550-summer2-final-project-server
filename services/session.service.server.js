7293dd8fd4920fee1dba321330a2b8e26e4a0ec7
Melina <sayeghmelina@gmail.com>
2018-08-11 16:43:52 -0400

    added sessions service

--- services/session.service.server.js
+++ services/session.service.server.js
@@ -1,17 +1,14 @@
 module.exports = app => {
-
     function setSession(req, res) {
         const name = req.params['name'];
         req.session[name] = req.params['value'];
         res.send(req.session);
     }
-
     function getSession(req, res) {
         const name = req.params['name'];
         const value = req.session[name];
         res.send(value);
     }
-
     function getSessionAll(req, res) {
         res.send(req.session);
     }
@@ -20,11 +17,9 @@ module.exports = app => {
         res.session.destroy();
         res.send(200);
     }
-
     app.get('/api/session/set/:name/:value', setSession);
     app.get('/api/session/get/:name',        getSession);
     app.get('/api/session/get',              getSessionAll);
     app.get('/api/session/reset',            resetSession);
-
 };
 
