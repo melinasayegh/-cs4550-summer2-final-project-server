5a3fead56fd7a306e5e419cdb8b87bd48e3238a0
ysmolyar <yonismolyar1@gmail.com>
2018-08-18 20:34:12 -0400

    most up to date recipe service

--- services/recipe.service.server.js
+++ services/recipe.service.server.js
@@ -2,13 +2,17 @@ const unirest = require('unirest');
 
 module.exports = function (app) {
 
-    app.get('/api/randomRecipe', getRandomRecipe);
+    app.get('/api/randomRecipe/:tag', getRandomRecipe);
 
     function getRandomRecipe(req, res) {
-         unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=dessert")
+        unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=" + req.params.tag)
             .header("X-Mashape-Key", "VsYAEwDWxwmshX990l6hWa2WtVNAp1f1zBojsnIEiyKW9hG6Sf")
             .header("Accept", "application/json")
-            .end(recipes => res.send(recipes.body.recipes[0]));
+            .end(recipes => {
+                if (recipes !== null) {
+                    res.send(recipes.body.recipes[0])
+                }
+            });
     }
 
 };
